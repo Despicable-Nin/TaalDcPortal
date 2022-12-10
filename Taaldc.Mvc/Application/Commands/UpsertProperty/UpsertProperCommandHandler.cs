9b992c6d95d36190ea15be2e74099a7b028f6ea1
Ninism <nin.alamo@outlook.com>
2022-12-10 14:27:36 +0800

    Update UpsertProperCommandHandler.cs

--- Taaldc.Mvc/Application/Commands/UpsertProperty/UpsertProperCommandHandler.cs
+++ Taaldc.Mvc/Application/Commands/UpsertProperty/UpsertProperCommandHandler.cs
@@ -2,7 +2,7 @@ using MediatR;
 
 namespace Taaldc.Mvc.Application.Commands.UpdateProject;
 
-public class UpsertProperCommandHandler : IRequestHandler<UpsertPropertyCommand, string>
+public class UpsertPropertyCommandHandler : IRequestHandler<UpsertPropertyCommand, string>
 {
     public Task<string> Handle(UpsertPropertyCommand request, CancellationToken cancellationToken)
     {
