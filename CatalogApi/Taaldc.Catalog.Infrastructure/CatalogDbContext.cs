0e6aed2b463b77b5d9c4275ab9308da4e31aec6f
Ninism <nin.alamo@outlook.com>
2022-12-14 21:23:55 +0800

    Added factory

--- CatalogApi/Taaldc.Catalog.Infrastructure/CatalogDbContext.cs
+++ CatalogApi/Taaldc.Catalog.Infrastructure/CatalogDbContext.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Taaldc.Catalog.Domain.AggregatesModel.CondoAggregate;
 using Taaldc.Catalog.Domain.SeedWork;
 
@@ -7,14 +9,21 @@ namespace Taaldc.Catalog.Infrastructure;
 public class CatalogDbContext : DbContext, IUnitOfWork
 {
     public const string DEFAULT_SCHEMA = "catalog";
-
+    private readonly IMediator _mediator;
  
     public DbSet<Project> Projects { get; set; }
 
     public CatalogDbContext(DbContextOptions options) : base(options)
     {
-        System.Diagnostics.Debug.WriteLine("CatalogDbContext::ctor ->" + this.GetHashCode());
+
     }
+    
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options, IMediator mediator) : base(options)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        System.Diagnostics.Debug.WriteLine("OrderingContext::ctor ->" + this.GetHashCode());
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -28,5 +37,49 @@ public class CatalogDbContext : DbContext, IUnitOfWork
     public Task<int> SaveEntitiesAsync(CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
+    }
+}
+
+public class CatalogDbContextDesignFactory : IDesignTimeDbContextFactory<CatalogDbContext>
+{
+    public CatalogDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CatalogDbContext>()
+            .UseSqlServer("Server=localhost;Database=taaldb;User Id=sa;Password=someThingComplicated1234;", sqlServerOptionsAction: x => x.MigrationsAssembly("Taaldc.Catalog.Infrastructure"));
+
+        return new CatalogDbContext(optionsBuilder.Options, new NoMediator());
+    }
+
+    class NoMediator : IMediator
+    {
+        public IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            return default(IAsyncEnumerable<TResponse>);
+        }
+
+        public IAsyncEnumerable<object?> CreateStream(object request, CancellationToken cancellationToken = default)
+        {
+            return default(IAsyncEnumerable<object?>);
+        }
+
+        public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default(CancellationToken)) where TNotification : INotification
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Publish(object notification, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult<TResponse>(default(TResponse));
+        }
+
+        public Task<object> Send(object request, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(default(object));
+        }
     }
 }
