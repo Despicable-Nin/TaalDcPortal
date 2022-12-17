863900843e96d38c40861b388bd0775cf128daf6
Ninism <nin.alamo@outlook.com>
2022-12-17 08:23:17 +0800

    UpsertProperty

--- Taaldc.Mvc/Application/Commands/UpsertProperty/UpsertPropertyCommand.cs
+++ Taaldc.Mvc/Application/Commands/UpsertProperty/UpsertPropertyCommand.cs
@@ -1,9 +1,11 @@
 using System.Runtime.Serialization;
+using FluentValidation;
 using MediatR;
+using Taaldc.Mvc.Application.Commands.UpsertProject;
 
 namespace Taaldc.Mvc.Application.Commands.UpsertProperty;
 
-public class UpsertPropertyCommand : IRequest<string>
+public class UpsertPropertyCommand : IRequest<CommandResult>
 {
     [DataMember] public int ProjectId { get; set; }
     [DataMember] public int? PropertyId { get; set; }
@@ -18,4 +20,14 @@ public class UpsertPropertyCommand : IRequest<string>
         PropertyId = propertyId;
     }
 
+}
+
+public class UpsertPropertyCommandValidator : AbstractValidator<UpsertPropertyCommand>
+{
+    public UpsertPropertyCommandValidator()
+    {
+        RuleFor(i => i.ProjectId).NotEmpty().WithMessage("No project id found.");
+        RuleFor(i => i.Name).NotEmpty().WithMessage("Name is required.");
+        RuleFor(i => i.LandArea).NotEmpty().WithMessage("Land area is required.");
+    }
 }
--- Taaldc.Mvc/Application/Commands/UpsertProperty/UpsertPropertyCommandHandler.cs
+++ Taaldc.Mvc/Application/Commands/UpsertProperty/UpsertPropertyCommandHandler.cs
@@ -1,11 +1,32 @@
 using MediatR;
+using Microsoft.VisualBasic;
+using Taaldc.Catalog.Domain.AggregatesModel.CondoAggregate;
+using Taaldc.Catalog.Infrastructure.Repositories;
 
 namespace Taaldc.Mvc.Application.Commands.UpsertProperty;
 
-public class UpsertPropertyCommandHandler : IRequestHandler<UpsertPropertyCommand, string>
+public class UpsertPropertyCommandHandler : IRequestHandler<UpsertPropertyCommand, CommandResult>
 {
-    public Task<string> Handle(UpsertPropertyCommand request, CancellationToken cancellationToken)
+    private readonly IProjectRepository _repository;
+
+    public UpsertPropertyCommandHandler(IProjectRepository repository)
     {
-        throw new NotImplementedException();
+        _repository = repository;
+    }
+
+    public async Task<CommandResult> Handle(UpsertPropertyCommand request, CancellationToken cancellationToken)
+    {
+        var project = await _repository.GetAsync(request.PropertyId.Value);
+
+        if (project == default) return CommandResult.Failed(request.PropertyId.Value, $"Project with Id:{request.ProjectId} does not exist.");
+
+        var property = project.UpsertProperty(request.Name, request.LandArea, request.PropertyId.Value);
+
+        _repository.Update(project);
+
+        await _repository.UnitOfWork.SaveChangesAsync(cancellationToken);
+        
+        return CommandResult.Success(property.Id);
+
     }
 }
