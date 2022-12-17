01eddc1fb24106f2846ca4e441aca27d3bcc2cb0
Ninism <nin.alamo@outlook.com>
2022-12-17 08:22:07 +0800

    Separated DbContextDesignFactory file from CatalogDbContext

--- CatalogApi/Taaldc.Catalog.Infrastructure/CatalogDbContext.cs
+++ CatalogApi/Taaldc.Catalog.Infrastructure/CatalogDbContext.cs
@@ -1,6 +1,5 @@
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Taaldc.Catalog.Domain.AggregatesModel.CondoAggregate;
 using Taaldc.Catalog.Domain.SeedWork;
 
@@ -12,8 +11,8 @@ public class CatalogDbContext : DbContext, IUnitOfWork
     private readonly IMediator _mediator;
  
     public DbSet<Project> Projects { get; set; }
-    
-    
+
+
     public CatalogDbContext(DbContextOptions<CatalogDbContext> options, IMediator mediator) : base(options)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
@@ -33,49 +32,5 @@ public class CatalogDbContext : DbContext, IUnitOfWork
     public Task<int> SaveEntitiesAsync(CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
-    }
-}
-
-public class CatalogDbContextDesignFactory : IDesignTimeDbContextFactory<CatalogDbContext>
-{
-    public CatalogDbContext CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<CatalogDbContext>()
-            .UseSqlServer("Server=localhost;Database=taaldb;User Id=sa;Password=someThingComplicated1234;", sqlServerOptionsAction: x => x.MigrationsAssembly("Taaldc.Catalog.Infrastructure"));
-
-        return new CatalogDbContext(optionsBuilder.Options, new NoMediator());
-    }
-
-    class NoMediator : IMediator
-    {
-        public IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request, CancellationToken cancellationToken = default)
-        {
-            return default(IAsyncEnumerable<TResponse>);
-        }
-
-        public IAsyncEnumerable<object?> CreateStream(object request, CancellationToken cancellationToken = default)
-        {
-            return default(IAsyncEnumerable<object?>);
-        }
-
-        public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default(CancellationToken)) where TNotification : INotification
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task Publish(object notification, CancellationToken cancellationToken = default)
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return Task.FromResult<TResponse>(default(TResponse));
-        }
-
-        public Task<object> Send(object request, CancellationToken cancellationToken = default)
-        {
-            return Task.FromResult(default(object));
-        }
     }
 }
