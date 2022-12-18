d75fdc56a1d0afff400498dc7ea286d7883de03c
Ninism <nin.alamo@outlook.com>
2022-12-18 08:07:45 +0800

    Upsert Floor

--- Taaldc.Mvc/Application/Commands/UpsertFloor/UpsertFloorCommand.cs
+++ Taaldc.Mvc/Application/Commands/UpsertFloor/UpsertFloorCommand.cs
@@ -5,18 +5,14 @@ namespace Taaldc.Mvc.Application.Commands.UpsertFloor;
 
 public class UpsertFloorCommand : IRequest<CommandResult>
 {
-    public UpsertFloorCommand(int projectId, int propertyId, int towerId, int? floorId, string name, string description)
+    public UpsertFloorCommand(int towerId, int? floorId, string name, string description)
     {
-        ProjectId = projectId;
-        PropertyId = propertyId;
         TowerId = towerId;
         FloorId = floorId;
         Name = name;
         Description = description;
     }
 
-    [DataMember] public int ProjectId { get; set; }
-    [DataMember] public int PropertyId { get; set; }
     [DataMember] public int TowerId { get; set; }
     [DataMember] public int? FloorId { get; set; }
     [DataMember] public string Name { get; set; }
--- Taaldc.Mvc/Application/Commands/UpsertFloor/UpsertFloorCommandHandler.cs
+++ Taaldc.Mvc/Application/Commands/UpsertFloor/UpsertFloorCommandHandler.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using taaldc_catalog.domain.Exceptions;
 using Taaldc.Catalog.Domain.AggregatesModel.FloorAggregate;
 
 namespace Taaldc.Mvc.Application.Commands.UpsertFloor;
@@ -6,9 +7,35 @@ namespace Taaldc.Mvc.Application.Commands.UpsertFloor;
 public class UpsertFloorCommandHandler : IRequestHandler<UpsertFloorCommand, CommandResult>
 {
     private readonly IFloorRepository _repository;
-    
+
+    public UpsertFloorCommandHandler(IFloorRepository repository)
+    {
+        _repository = repository;
+    }
+
     public async Task<CommandResult> Handle(UpsertFloorCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        Floor floor = default;
+        if (request.FloorId.HasValue)
+        {
+            floor = await _repository.GetAsync(request.FloorId.Value);
+            
+            if (floor == null)
+                throw new CatalogDomainException(nameof(request.FloorId),
+                    new KeyNotFoundException($"Floor with id:{request.FloorId} not found."));
+
+            floor.Update(request.Name, request.Description);
+
+            _repository.Update(floor);
+        }
+        else
+        {
+            floor = new(request.Name, request.Description);
+            _repository.Add(floor);
+        }
+
+        await _repository.UnitOfWork.SaveChangesAsync(cancellationToken);
+        
+        return CommandResult.Success(floor.Id);
     }
 }
