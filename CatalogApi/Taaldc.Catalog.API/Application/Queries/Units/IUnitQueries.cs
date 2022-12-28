b400492ffbb6f27d486b75e6f9deef8f8048fa59
Ninism <nin.alamo@outlook.com>
2022-12-28 19:44:24 +0800

    Update IUnitQueries.cs

--- CatalogApi/Taaldc.Catalog.API/Application/Queries/Units/IUnitQueries.cs
+++ CatalogApi/Taaldc.Catalog.API/Application/Queries/Units/IUnitQueries.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Taaldc.Catalog.API.Application.Queries.Units;
@@ -13,70 +12,62 @@ public interface IUnitQueries
 
 public class UnitQueries : IUnitQueries
 {
-    private readonly string _connectionstring;
+    private readonly string _connectionString;
 
     public UnitQueries(string connectionString)
     {
-        _connectionstring = string.IsNullOrWhiteSpace(connectionString) ? throw new ArgumentNullException(nameof(connectionString)) : connectionString;
+        _connectionString = string.IsNullOrWhiteSpace(connectionString)
+            ? throw new ArgumentNullException(nameof(connectionString))
+            : connectionString;
     }
-    
-    public async Task<AvailableUnitQueryResult> GetAvailableUnitsAsync(int? unitTypeId, int? viewId, int? floorId, int min = 0,
+
+    public async Task<AvailableUnitQueryResult> GetAvailableUnitsAsync(int? unitTypeId, int? viewId, int? floorId,
+        int min = 0,
         int max = 999999999, int pageSize = 20, int pageNumber = 1)
     {
-        string query =
+        var query =
             " SELECT U.[Id], U.[Identifier], U.[Price], U.[FloorArea], F.Name [Floor], F.[Description] [FloorDesc], S.Name [View], US.Name [Status], UT.Name [Type], UT.ShortCode [TypeCode] ";
         query += " FROM [taaldb_admin].[catalog].[unit] U JOIN [taaldb_admin].[catalog].floors F ON U.FloorId = F.Id ";
         query += " JOIN [taaldb_admin].[catalog].scenicview S ON U.ScenicViewId = S.Id ";
         query += " JOIN [taaldb_admin].[catalog].unitstatus US ON U.UnitStatus = US.Id ";
         query += " JOIN taaldb_admin.[catalog].unittype UT ON U.UnitType = UT.Id ";
 
-        var clauses = new List<string>();
-      
+        List<string> clauses = new();
+
         if (unitTypeId.HasValue)
         {
             clauses.Add($"UT.Id = {unitTypeId.Value}");
-        }
 
-        if (viewId.HasValue)
-        {
-            clauses.Add($"S.Id = {viewId.Value}");
-        }
-        
-        if (floorId.HasValue)
-        {
-            clauses.Add($"F.Id = {floorId.Value}");
-        }
 
-      
+            if (viewId.HasValue) clauses.Add($"S.Id = {viewId.Value}");
+
+            if (floorId.HasValue) clauses.Add($"F.Id = {floorId.Value}");
+
             clauses.Add($"(U.Price BETWEEN {min} AND {max})");
-        
-        
-        clauses.Add($"US.Id = 1");
+            clauses.Add("US.Id = 1");
 
-        var where = string.Empty;
-        if (clauses.Any())
-        {
-            where = " WHERE " + string.Join(" AND ", clauses.ToArray());
+            var where = string.Empty;
+            if (clauses.Any()) where = " WHERE " + string.Join(" AND ", clauses.ToArray());
+
+            if (!string.IsNullOrWhiteSpace(where)) query += where;
+
+            query += $" ORDER BY U.Id OFFSET {(pageNumber - 1) * pageSize} ROWS FETCH NEXT {pageSize} ROWS ONLY ";
+
+            await using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync(CancellationToken.None);
+
+            var result = await connection.QueryAsync<AvailableUnit>(query);
+
+            var countQuery = " SELECT COUNT(*) [Total] ";
+            countQuery +=
+                " FROM [taaldb_admin].[catalog].[unit] U JOIN [taaldb_admin].[catalog].floors F ON U.FloorId = F.Id ";
+            countQuery += " JOIN [taaldb_admin].[catalog].scenicview S ON U.ScenicViewId = S.Id ";
+            countQuery += " JOIN [taaldb_admin].[catalog].unitstatus US ON U.UnitStatus = US.Id ";
+            countQuery += " JOIN taaldb_admin.[catalog].unittype UT ON U.UnitType = UT.Id ";
+            countQuery += where;
+
+            var temp = await connection.QueryAsync<int>(countQuery);
+
+            return new AvailableUnitQueryResult(pageSize, pageNumber, temp.Single(), result.ToArray());
         }
-
-        if (!string.IsNullOrWhiteSpace(where)) query += where;
-        
-        query += $" ORDER BY U.Id OFFSET {(pageNumber - 1) * pageSize} ROWS FETCH NEXT {pageSize} ROWS ONLY ";
-
-        using var connection = new SqlConnection(_connectionstring);
-        await connection.OpenAsync(CancellationToken.None);
-
-        var result = await connection.QueryAsync<AvailableUnit>(query);
-        
-        string countQuery = " SELECT COUNT(*) [Total] ";
-        countQuery += " FROM [taaldb_admin].[catalog].[unit] U JOIN [taaldb_admin].[catalog].floors F ON U.FloorId = F.Id ";
-        countQuery += " JOIN [taaldb_admin].[catalog].scenicview S ON U.ScenicViewId = S.Id ";
-        countQuery += " JOIN [taaldb_admin].[catalog].unitstatus US ON U.UnitStatus = US.Id ";
-        countQuery += " JOIN taaldb_admin.[catalog].unittype UT ON U.UnitType = UT.Id ";
-        countQuery += where;
-
-        var temp = await connection.QueryAsync<int>(countQuery);
-
-        return new AvailableUnitQueryResult(pageSize, pageNumber, temp.First(), result.ToArray());
-    }   
-}
+    }
