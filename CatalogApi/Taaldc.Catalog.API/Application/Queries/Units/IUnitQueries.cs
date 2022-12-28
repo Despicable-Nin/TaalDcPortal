d0bf2a4ffcddc1bcc360afeafb36708701f5482e
Ninism <nin.alamo@outlook.com>
2022-12-28 20:02:06 +0800

    Update IUnitQueries.cs
    Fixed error in brackets

--- CatalogApi/Taaldc.Catalog.API/Application/Queries/Units/IUnitQueries.cs
+++ CatalogApi/Taaldc.Catalog.API/Application/Queries/Units/IUnitQueries.cs
@@ -34,41 +34,39 @@ public class UnitQueries : IUnitQueries
 
         List<string> clauses = new();
 
-        if (unitTypeId.HasValue)
-        {
-            clauses.Add($"UT.Id = {unitTypeId.Value}");
+        if (unitTypeId.HasValue) clauses.Add($"UT.Id = {unitTypeId.Value}");
 
 
-            if (viewId.HasValue) clauses.Add($"S.Id = {viewId.Value}");
+        if (viewId.HasValue) clauses.Add($"S.Id = {viewId.Value}");
 
-            if (floorId.HasValue) clauses.Add($"F.Id = {floorId.Value}");
+        if (floorId.HasValue) clauses.Add($"F.Id = {floorId.Value}");
 
-            clauses.Add($"(U.Price BETWEEN {min} AND {max})");
-            clauses.Add("US.Id = 1");
+        clauses.Add($"(U.Price BETWEEN {min} AND {max})");
+        clauses.Add("US.Id = 1");
 
-            var where = string.Empty;
-            if (clauses.Any()) where = " WHERE " + string.Join(" AND ", clauses.ToArray());
+        var where = string.Empty;
+        if (clauses.Any()) where = " WHERE " + string.Join(" AND ", clauses.ToArray());
 
-            if (!string.IsNullOrWhiteSpace(where)) query += where;
+        if (!string.IsNullOrWhiteSpace(where)) query += where;
 
-            query += $" ORDER BY U.Id OFFSET {(pageNumber - 1) * pageSize} ROWS FETCH NEXT {pageSize} ROWS ONLY ";
+        query += $" ORDER BY U.Id OFFSET {(pageNumber - 1) * pageSize} ROWS FETCH NEXT {pageSize} ROWS ONLY ";
 
-            await using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync(CancellationToken.None);
+        await using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync(CancellationToken.None);
 
-            var result = await connection.QueryAsync<AvailableUnit>(query);
+        var result = await connection.QueryAsync<AvailableUnit>(query);
 
-            var countQuery = " SELECT COUNT(*) [Total] ";
-            countQuery +=
-                " FROM [taaldb_admin].[catalog].[unit] U JOIN [taaldb_admin].[catalog].floors F ON U.FloorId = F.Id ";
-            countQuery += " JOIN [taaldb_admin].[catalog].scenicview S ON U.ScenicViewId = S.Id ";
-            countQuery += " JOIN [taaldb_admin].[catalog].unitstatus US ON U.UnitStatus = US.Id ";
-            countQuery += " JOIN taaldb_admin.[catalog].unittype UT ON U.UnitType = UT.Id ";
-            countQuery += where;
+        var countQuery = " SELECT COUNT(*) [Total] ";
+        countQuery +=
+            " FROM [taaldb_admin].[catalog].[unit] U JOIN [taaldb_admin].[catalog].floors F ON U.FloorId = F.Id ";
+        countQuery += " JOIN [taaldb_admin].[catalog].scenicview S ON U.ScenicViewId = S.Id ";
+        countQuery += " JOIN [taaldb_admin].[catalog].unitstatus US ON U.UnitStatus = US.Id ";
+        countQuery += " JOIN taaldb_admin.[catalog].unittype UT ON U.UnitType = UT.Id ";
+        countQuery += where;
 
-            var temp = await connection.QueryAsync<int>(countQuery);
+        var temp = await connection.QueryAsync<int>(countQuery);
 
-            return new AvailableUnitQueryResult(pageSize, pageNumber, temp.Single(), result.ToArray());
-        }
+        return new AvailableUnitQueryResult(pageSize, pageNumber, temp.Single(), result.ToArray());
+    
     }
 }
