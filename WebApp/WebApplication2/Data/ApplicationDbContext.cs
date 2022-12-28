2f4b9a10bcfd3fd9f46b0eabc22fc84e24a3d44b
Ninism <nin.alamo@outlook.com>
2022-12-28 22:19:35 +0800

    Initial portal

--- WebApp/WebApplication2/Data/ApplicationDbContext.cs
+++ WebApp/WebApplication2/Data/ApplicationDbContext.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication2.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<AppUser> AppUsers { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
+}
+
+public class AppUser : IdentityUser<int>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
