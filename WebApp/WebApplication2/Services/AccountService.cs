5fc9697ad206a52306ec6f5e043ad1e4ea70af02
Ninism <nin.alamo@outlook.com>
2022-12-30 19:57:51 +0800

    Create AccountService.cs

---
+++ WebApp/WebApplication2/Services/AccountService.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.ViewModels.Users;
+
+namespace WebApplication2.Services;
+
+public class AccountService : IAccountService
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private ILogger<AccountService> _logger;
+
+
+    public AccountService(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ILogger<AccountService> logger)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _logger = logger;
+    }
+
+    public async Task<UserIndexViewModel> GetListOfUsersWithRoles()
+    {
+        var users = await _userManager.Users.AsNoTracking().ToListAsync();
+        var roles = await _roleManager.Roles.AsNoTracking().ToArrayAsync();
+
+        UserIndexViewModel vm = new();
+
+        users.ForEach( i =>
+        {
+            var currentRoles = new List<string>();
+            foreach (var r in roles)
+            {
+                if ( _userManager.IsInRoleAsync(i, r.Name).Result)
+                {
+                    currentRoles.Add(r.Name);
+                }
+            }
+            vm.AddUser(new UserViewModel(i.Email,  currentRoles.ToArray()));
+        });
+
+        return vm;
+    }
+}
+
+public interface IAccountService
+{
+    Task<UserIndexViewModel> GetListOfUsersWithRoles();
+}
