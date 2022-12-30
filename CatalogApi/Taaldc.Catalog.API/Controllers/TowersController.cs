6b37946529861a5d3610bea7a12c25d27016d534
Dan Evalla <chrisdanevalla@gmail.com>
2022-12-30 14:28:46 +0800

    Available Units By Type Query

--- CatalogApi/Taaldc.Catalog.API/Controllers/TowersController.cs
+++ CatalogApi/Taaldc.Catalog.API/Controllers/TowersController.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Taaldc.Catalog.API.Application.Commands.UpsertFloor;
 using Taaldc.Catalog.API.Application.Commands.UpsertTower;
+using Taaldc.Catalog.API.Application.Queries;
 using Taaldc.Catalog.API.DTO;
 using Taaldc.Catalog.Domain.AggregatesModel.ProjectAggregate;
 
@@ -9,8 +10,11 @@ namespace Taaldc.Catalog.API.Controllers;
 
 public class TowersController : ApiBaseController<TowersController>
 {
-    public TowersController(ILogger<TowersController> logger, IMediator mediator) : base(logger, mediator)
+	private readonly IUnitQueries _unitQueries;
+
+	public TowersController(ILogger<TowersController> logger, IMediator mediator, IUnitQueries unitQueries) : base(logger, mediator)
     {
+        _unitQueries = unitQueries;
     }
     
     [HttpPost]
@@ -20,4 +24,12 @@ public class TowersController : ApiBaseController<TowersController>
     {
         return Ok(await _mediator.Send(new UpsertTowerCommand(model.PropertyId, model.Name, model.Address, model.TowerId)));
     }
+
+	[HttpGet("{id}/available-units-by-type")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesErrorResponseType(typeof(BadRequestResult))]
+	public async Task<IActionResult> GetAvailabilityByUnitType(int id)
+	{
+		return Ok(await _unitQueries.GetUnitTypeAvailabilityByTowerId(id));
+	}
 }
