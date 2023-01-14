0d77963b680bb3198d56a06a278a78281177e6ba
Ninism <nin.alamo@outlook.com>
2023-01-14 10:25:06 +0800

    Setup for factory

--- SalesApi/Taaldc.Sales.Api/Infrastructure/Factories/SalesDbContextFactory.cs
+++ SalesApi/Taaldc.Sales.Api/Infrastructure/Factories/SalesDbContextFactory.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Taaldc.Catalog.Infrastructure;
+using Taaldc.Sales.Infrastructure;
 
-namespace Taaldc.Catalog.API.Infrastructure.Factories;
+namespace Taaldc.Sales.Api.Infrastructure.Factories;
 
-public class CatalogDbContextFactory : IDesignTimeDbContextFactory<CatalogDbContext>
+public class SalesDbContextFactory : IDesignTimeDbContextFactory<SalesDbContext>
 {
-    public CatalogDbContext CreateDbContext(string[] args)
+    public SalesDbContext CreateDbContext(string[] args)
     {
         var config = new ConfigurationBuilder()
             .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
@@ -16,9 +16,9 @@ public class CatalogDbContextFactory : IDesignTimeDbContextFactory<CatalogDbCont
 
         var connectionString = config.GetConnectionString("DefaultConnection");
 
-        var optionsBuilder = new DbContextOptionsBuilder<CatalogDbContext>();
+        var optionsBuilder = new DbContextOptionsBuilder<SalesDbContext>();
         optionsBuilder.UseSqlServer(connectionString, x => x.MigrationsAssembly("Taaldc.Catalog.Infrastructure"));
 
-        return new CatalogDbContext(optionsBuilder.Options, new NoMediator());
+        return new SalesDbContext(optionsBuilder.Options, new NoMediator());
     }
 }
--- SalesApi/Taaldc.Sales.Domain/AggregatesModel/UnitAggregate/IUnitReplicaRepository.cs
+++ SalesApi/Taaldc.Sales.Domain/AggregatesModel/UnitAggregate/IUnitReplicaRepository.cs
@@ -5,4 +5,5 @@ namespace Taaldc.Sales.Domain.AggregatesModel.BuyerAggregate;
 public interface IUnitReplicaRepository : IRepository<UnitReplica>
 {
     Task<UnitReplica> AddASync(UnitReplica unitReplica);
+    
 }
