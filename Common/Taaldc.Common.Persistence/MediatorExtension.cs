7ee8fc56119e374b3ae45103217b6646c9bcafd8
Ninism <nin.alamo@outlook.com>
2023-01-21 21:15:17 +0800

    Domain Event

---
+++ Common/Taaldc.Common.Persistence/MediatorExtension.cs
@@ -0,0 +1,25 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SeedWork;
+
+namespace Taaldc.Common.Persistence;
+
+static class MediatorExtension
+{
+    public static async Task DispatchDomainEventsAsync(this IMediator mediator, DbContext ctx)
+    {
+        var domainEntities = ctx.ChangeTracker
+            .Entries<DomainEntity>()
+            .Where(x => x.Entity.DomainEvents != null && x.Entity.DomainEvents.Any());
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToList();
+
+        domainEntities.ToList()
+            .ForEach(entity => entity.Entity.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await mediator.Publish(domainEvent);
+    }
+}
