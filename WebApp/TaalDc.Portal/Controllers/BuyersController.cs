4e5be072ced38b0b6c219f84af882a59669a6bc3
Chrisdan Evalla <chrisdanevalla@gmail.com>
2023-02-08 04:17:27 +0800

    buyers

---
+++ WebApp/TaalDc.Portal/Controllers/BuyersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaalDc.Portal.Services;
+using WebApplication2.Controllers;
+
+namespace TaalDc.Portal.Controllers
+{
+    [Authorize]
+    public class BuyersController : Controller
+    {
+        private readonly ILogger<BuyersController> _logger;
+        public BuyersController(ILogger<BuyersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult QuickCreate()
+        {
+            return View();
+        }
+    }
+}
