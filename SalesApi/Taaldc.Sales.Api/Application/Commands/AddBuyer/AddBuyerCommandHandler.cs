67ff336e0ea916f08dc691c93fc2d8e4deddffe4
Ninism <nin.alamo@outlook.com>
2023-02-19 06:34:03 +0800

    Update AddBuyerCommandHandler.cs
    Added logging

--- SalesApi/Taaldc.Sales.Api/Application/Commands/AddBuyer/AddBuyerCommandHandler.cs
+++ SalesApi/Taaldc.Sales.Api/Application/Commands/AddBuyer/AddBuyerCommandHandler.cs
@@ -20,30 +20,37 @@ public class AddBuyerCommandHandler : IRequestHandler<AddBuyerCommand, int>
 
     public async Task<int> Handle(AddBuyerCommand request, CancellationToken cancellationToken)
     {
-        
-        _logger.LogInformation("Start adding 'Buyer' entity.");
-        Buyer buyer = default; //default = null, it's type safe
-        
-        _logger.LogInformation($"'Check if Buyer's email has been used.");
-        buyer =await  _buyerRepository.GetByEmailAsync(request.EmailAddress);
-
-        if (buyer != default)
+        try
         {
-            throw new SalesDomainException(nameof(AddBuyerCommandHandler), new Exception("Email has already been registered."));
+            _logger.LogInformation("Start adding 'Buyer' entity.");
+            Buyer buyer = default; //default = null, it's type safe
+
+            _logger.LogInformation($"'Check if Buyer's email has been used.");
+            buyer = await _buyerRepository.GetByEmailAsync(request.EmailAddress);
+
+            if (buyer != default)
+            {
+                throw new SalesDomainException(nameof(AddBuyerCommandHandler),
+                    new Exception("Email has already been registered."));
+            }
+
+            buyer = new Buyer(request.Salutation, request.FirstName, request.MiddleName, request.LastName,
+                request.EmailAddress, request.PhoneNo, request.MobileNo, request.DoB, request.CivilStatusId, null,
+                request.IsCorporate,
+                null);
+
+            _logger.LogInformation($"Upsert address -- replace if same type exists,otherwise just add.");
+            buyer.UpsertAddress(request.HomeAddress.ToEntity());
+
+            _logger.LogInformation($"Update Company -- replace if exists, otherwise just add.");
+            buyer.UpdateCompany(request.Company.ToEntity());
+
+            return request.BuyerId.Value;
         }
-
-        buyer = new Buyer(request.Salutation, request.FirstName, request.MiddleName, request.LastName,
-            request.EmailAddress, request.PhoneNo, request.MobileNo, request.DoB,request.CivilStatusId, null, request.IsCorporate,
-            null);
-        
-        buyer.UpsertAddress(request.HomeAddress.ToEntity());
-        
-        buyer.UpdateCompany(request.Company.ToEntity());
-        
-        return request.BuyerId.Value;
+        catch (Exception ex)
+        {
+            _logger.LogError(ex,nameof(AddBuyerCommandHandler.Handle), new object[]{request,ex.InnerException});
+            throw;
+        }
     }
-    
-   
-
-
 }
