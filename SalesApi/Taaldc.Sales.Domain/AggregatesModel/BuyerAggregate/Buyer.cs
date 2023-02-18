b6d461ed4e95a2f0ac42d80ddc3c0c07dd4cb712
Ninism <nin.alamo@outlook.com>
2023-02-18 23:37:29 +0800

    Update Buyer.cs

--- SalesApi/Taaldc.Sales.Domain/AggregatesModel/BuyerAggregate/Buyer.cs
+++ SalesApi/Taaldc.Sales.Domain/AggregatesModel/BuyerAggregate/Buyer.cs
@@ -5,6 +5,7 @@ namespace Taaldc.Sales.Domain.AggregatesModel.BuyerAggregate;
 
 public class Buyer : Entity, IAggregateRoot
 {
+    #region Ctor
     public Buyer(string salutation,
         string firstName,
         string middleName,
@@ -38,35 +39,62 @@ public class Buyer : Entity, IAggregateRoot
     {
         _addresses = new List<Address>();
     }
+    #endregion 
 
+    #region Personal Info
     public string Salutation { get; private set; }
     public string FirstName { get; private set; }
     public string MiddleName { get; private set; }
     public string LastName { get; private set; }
     public DateTime DoB { get; private set; }
     
+    
     private int _civilStatusId;
     public CivilStatus CivilStatus { get; private set; }
+    
+    public bool IsCorporate { get; private set; }
+    
+    //value object
+    public Company? Company { get; private set; }
+    
+    #endregion
+    
+    #region Contact information
 
     public string EmailAddress { get; private set; }
     public string MobileNo { get; private set; }
     public string PhoneNo { get; private set; }
+    
+    #endregion
 
+    #region Misc
     public string Occupation { get; private set; }
     public string Tin { get; private set; }
     public string GovIssuedId { get; private set; }
     public DateTime? GovIssuedIdValidUntil { get; private set; }
-
-    public int? SpouseId { get; private set; }
-
-    public bool IsCorporate { get; private set; }
-
-    //private int? _companyId;
-    public Company? Company { get; private set; }
+    
+    #endregion
+    
+    
+    #region Familiar Affiliate
+    public int? PartnerId { get; private set; }
+    
+    #endregion
+    
+    #region Address
 
     private List<Address> _addresses;
     public IReadOnlyCollection<Address> Addresses => _addresses.AsReadOnly();
+    
+    #endregion
 
+
+    public void AddPartnerOrSpouse(int buyerIdOfPartnerOrSpouse)
+    {
+        PartnerId = buyerIdOfPartnerOrSpouse;
+    }
+
+    public void RemovePartnerOrSpouse() => PartnerId = default;
 
     public void UpdateBuyer(string salutation, string firstName, string middleName, string lastName, DateTime doB,
         int civilStatusId)
@@ -76,7 +104,7 @@ public class Buyer : Entity, IAggregateRoot
         MiddleName = middleName;
         LastName = lastName;
         DoB = doB;
-        //CivilStatusId = civilStatusId;
+        _civilStatusId = civilStatusId;
     }
 
     public void UpdateContactDetails(string emailAddress, string phoneNo, string mobileNo)
@@ -86,13 +114,32 @@ public class Buyer : Entity, IAggregateRoot
         MobileNo = mobileNo;
     }
 
-    public void UpdateIDInformation(string occupation, string tin, string govIssuedID, DateTime govIssuedIDValidUntil)
+    public void UpdateMiscInformation(string occupation, string tin, string govIssuedId, DateTime govIssuedIdValidUntil)
     {
         Occupation = occupation;
         Tin = tin;
-        GovIssuedId = govIssuedID;
-        GovIssuedIdValidUntil = govIssuedIDValidUntil;
+        GovIssuedId = govIssuedId;
+        GovIssuedIdValidUntil = govIssuedIdValidUntil;
     }
+    
+    public void UpsertAddress(Address newaddress)
+    {
+        var address = _addresses.SingleOrDefault(i => i.Type == newaddress.Type);
+
+        if (_addresses != default)
+        {
+            //remove first
+            _addresses.Remove(address);
+        }
+
+        _addresses.Add(newaddress);
+    }
+
+    public Address GetHomeAddress() => _addresses.FirstOrDefault(i => i.Type == AddressTypeEnum.Home);
+    public Address GetBillingAddress() => _addresses.FirstOrDefault(i => i.Type == AddressTypeEnum.Billing);
+    public Address GetBusinessAddress() => _addresses.FirstOrDefault(i => i.Type == AddressTypeEnum.Business);
+
+    public void UpdateCompany(Company company) => Company = company;
 
 
 }
