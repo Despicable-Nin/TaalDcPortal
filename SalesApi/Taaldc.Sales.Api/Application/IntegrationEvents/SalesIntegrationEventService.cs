cb21b119880464785aec7037797c1375e06e2912
Ninism <nin.alamo@outlook.com>
2023-02-20 23:24:02 +0800

    Update SalesIntegrationEventService.cs

--- SalesApi/Taaldc.Sales.Api/Application/IntegrationEvents/SalesIntegrationEventService.cs
+++ SalesApi/Taaldc.Sales.Api/Application/IntegrationEvents/SalesIntegrationEventService.cs
@@ -1,6 +1,7 @@
 using System.Data.Common;
 using EventBus.Abstractions;
 using EventBus.Events;
+using IntegrationEventLogEF;
 using IntegrationEventLogEF.Services;
 using Microsoft.EntityFrameworkCore;
 using Taaldc.Sales.Infrastructure;
@@ -11,20 +12,28 @@ public class SalesIntegrationEventService : ISalesIntegrationEventService
 {
     private readonly Func<DbConnection, IIntegrationEventLogService> _integrationEventLogServiceFactory;
     private readonly IEventBus _eventBus;
+    
+    //we need this for the purpose of tracking the current transactions happening on SALES
+    //-- this can have the same implementation on other services and their own dbcontex
     private readonly SalesDbContext _salesDbContext;
+    
+    //this lies within the sme database as the data context (salesdbcontext)
+    private readonly IntegrationEventLogContext _integrationEventLogContext;
+    
     private readonly IIntegrationEventLogService _eventLogService;
     private readonly ILogger<SalesIntegrationEventService> _logger;
 
-    public SalesIntegrationEventService(
-        Func<DbConnection, IIntegrationEventLogService> integrationEventLogServiceFactory,
-        IEventBus eventBus,
+    public SalesIntegrationEventService(IEventBus eventBus,
         SalesDbContext salesDbContext,
+        IntegrationEventLogContext eventLogContext,
+        Func<DbConnection, IIntegrationEventLogService> integrationEventLogServiceFactory,
         ILogger<SalesIntegrationEventService> logger)
     {
-        _integrationEventLogServiceFactory = integrationEventLogServiceFactory ??
-                                             throw new ArgumentNullException(nameof(integrationEventLogServiceFactory));
-        _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
         _salesDbContext = salesDbContext ?? throw new ArgumentNullException(nameof(salesDbContext));
+        _integrationEventLogServiceFactory = integrationEventLogServiceFactory ?? throw new ArgumentNullException(nameof(integrationEventLogServiceFactory));
+        _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+        
+        //we are using connection string from the salesdbcontext --> since they are of the same database
         _eventLogService = _integrationEventLogServiceFactory(_salesDbContext.Database.GetDbConnection());
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
