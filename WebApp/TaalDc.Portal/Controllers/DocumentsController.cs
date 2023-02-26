512388d9f489b28c40c8d8441dbed3b3e804597e
Chrisdan Evalla <49030263+evallachrisdan@users.noreply.github.com>
2023-02-26 23:24:32 +0800

    contract dto

--- WebApp/TaalDc.Portal/Controllers/DocumentsController.cs
+++ WebApp/TaalDc.Portal/Controllers/DocumentsController.cs
@@ -4,10 +4,17 @@ namespace TaalDc.Portal.Controllers
 {
     public class DocumentsController : Controller
     {
-        public IActionResult AcknowledgmentReceipt()
+
+        [HttpPost]
+        public async Task<IActionResult> AcknowledgmentReceipt([FromServices] IWebHostEnvironment env)
         {
-            ViewData["Layout"] = null;
-            return View();
+            // Get the path to the wwwroot folder
+            var wwwrootPath = env.WebRootPath;
+
+            string pdfTemplate = Path.Combine(wwwrootPath + "\\files\\forms\\ACKNOWLEDGMENT PDF.pdf");
+            var dataDir = Path.Combine(wwwrootPath + "\\files\\buyers-form\\");
+
+            return Ok();
         }
     }
 }
---
+++ WebApp/TaalDc.Portal/DTO/Sales/Contracts/Contract_ClientDto.cs
@@ -0,0 +1,16 @@
+﻿namespace TaalDc.Portal.DTO.Sales.Contracts
+{
+	public record Contract_ClientDto
+	{
+		public int Id { get; set; }
+		public string Code { get; set; }
+		public int BuyerId { get; set; }
+		public DateTime TransactionDate { get; set; }
+		public string Broker { get; set; }
+		public string PaymentMethod { get; set; }
+		public string Remarks { get; set; }
+
+		public IEnumerable<ContractOrderItem_ClientDto> OrderItems { get; set; }
+
+	}
+}
