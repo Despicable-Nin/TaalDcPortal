eee21b2ac16c5498761144a3650fded8c06b43f4
Ninism <nin.alamo@outlook.com>
2023-02-28 21:14:49 +0800

    CuurentUser

--- SalesApi/Taaldc.Sales.Api/CurrentUser.cs
+++ SalesApi/Taaldc.Sales.Api/CurrentUser.cs
@@ -15,8 +15,19 @@ public class CurrentUser : IAmCurrentUser
         IdentityId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
         Roles = httpContextAccessor.HttpContext?.User?.FindAll(ClaimTypes.Role)?.Select(i => i.Value).ToArray();
         _isAuthenticated = !string.IsNullOrEmpty(Email);
+        
+        var data = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.UserData);
+        if (data != default)
+        {
+            _company = data.Split("-")[0];
+            _prcLicense = data.Split("-")[1];
+        }
     }
-
+    
+    private readonly string _company;
+    private readonly string _prcLicense;
+    public string GetCompany() => _company;
+    public string GetPrcLicense() => _prcLicense;
     public string Name { get; }
     public string[] Roles { get; }
     public string Email { get; }
