9f41d9c66abf0548376d4cfba6b3560750a89d47
Ninism <nin.alamo@outlook.com>
2023-02-28 15:08:11 +0800

    migration

--- WebApp/TaalDc.Portal/Migrations/20230228070752_usrbroerk.Designer.cs
+++ WebApp/TaalDc.Portal/Migrations/20230228070752_usrbroerk.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaalDc.Portal.Data;
 
@@ -11,9 +12,11 @@ using TaalDc.Portal.Data;
 namespace TaalDc.Portal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230228070752_usrbroerk")]
+    partial class usrbroerk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
